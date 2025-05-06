d02395b27c061ac701e936884bff9366fadfe641
harish.k3 <harish.k@pixdynamics.com>
2025-05-06 09:20:21 +0530

    Directory structure changes

---
+++ data_injection/injection_pipeline.py
@@ -0,0 +1,21 @@
+import os
+import tempfile
+from typing import List
+from dotenv import load_dotenv
+from langchain_core.documents import Document
+from langchain_community.document_loaders import PyPDFLoader, Docx2txtLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_pinecone import PineconeVectorStore
+from utils.model_loaders import ModelLoader
+from utils.config_loader import load_config
+from pinecone import ServerlessSpec, Pinecone
+from uuid import uuid4
+import sys
+from exception.exceptions import TradingBotException
+
+
+
+
+class DataInjection:
+    def __init__(self):
+        pass
